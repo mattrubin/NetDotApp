c5c8cb00a68133ebf2baeaf3155b02a7c26807df
Matt Rubin <mattrubin@me.com>
2013-01-24 23:06:20 -0800

    Initial Commit

---
+++ NetDotApp/NDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NDAAppDelegate.h
+//  NetDotApp
+//
+//  Created by Me on 1/24/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetDotApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetDotApp
+//
+//  Created by Me on 1/24/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NDAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NDAAppDelegate class]));
+    }
+}
