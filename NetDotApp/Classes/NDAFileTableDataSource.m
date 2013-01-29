ffca4bfdc2b79c0d8c61a7f031b5195846cf8b63
Matt Rubin <mattrubin@me.com>
2013-01-29 01:12:37 -0800

    Swipe-to-delete on file cells

--- NetDotApp/Classes/NDAFileTableDataSource.m
+++ NetDotApp/Classes/NDAFileTableDataSource.m
@@ -137,28 +137,34 @@
     return [self.files objectAtIndex:indexPath.row];
 }
 
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
 
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
+
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+     // Return NO if you do not want the specified item to be editable.
+     return YES;
+}
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        ADNFile *file = [self fileForRowAtIndexPath:indexPath];
+        [[ADNClient sharedClient] deleteFile:file.fileId withCompletionHandler:^(ADNFile *file, ADNMetadata *meta, NSError *error) {
+            NSLog(@"meta: %@", meta);
+            if (!error) {
+                [self.files removeObjectAtIndex:indexPath.row];
+                [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            }
+        }];
+    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+}
+ 
 
 /*
  // Override to support rearranging the table view.
